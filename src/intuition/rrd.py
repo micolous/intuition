a63943553f6256b8246728646cc7903418e44d47
Michael Farrell <micolous+git@gmail.com>
2013-06-30 20:16:17 +0930

    ...

--- src/intuition/rrd.py
+++ src/intuition/rrd.py
@@ -46,7 +46,7 @@ class RrdOwlProtocol(OwlIntuitionProtocol):
 		
 		o = ['N']
 		for channel in chan_names:
-			o.append(str(msg.channels[channel].current_w.to_integral_value())))
+			o.append(str(msg.channels[channel].current_w.to_integral_value()))
 			o.append(str(msg.channels[channel].daily_wh.to_integral_value()))
 		
 		o = ':'.join(o)
